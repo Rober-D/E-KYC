29068f03cc5f9c35cf7cc15ae3ab10c03f18b344
Rober-D <Roberdanial06@gmail.com>
2024-05-15 14:25:21 +0300

    Add Splash Screen. Add NationalIDImageData Entity and Model. Handling Exceptions and errors in OCR-NationalID.

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,7 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
+  // Add Splash Screen
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
